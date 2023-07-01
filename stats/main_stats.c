4b3feca5f10a34d99274becf769b228cb1ad91a9
Diego <reisdiego144@gmail.com>
2023-07-01 20:05:45 -0300

    consertando bug de leitura de arquivo

--- stats/main_stats.c
+++ stats/main_stats.c
@@ -3,23 +3,39 @@
 #include <time.h>
 
 #include "../src/btree.h"
+
+#define SEARCH_NUM 30
+#define NUM_ELEM 10000
+
 /* Esse programa compara o tempo de pesquisa usando busca linear
  * e utilizando os indices da b-tree
  * */
 int main ()
 {
+  srand(time(NULL));
+  double mean = 0.0, time_spent; 
+  int randN;
   FILE *f = fopen(FILE_NAME, "r");
 
-  clock_t begin = clock();
-    // chama a funçao aqui
-    printLineLinear(f, 6059);
-  clock_t end = clock();
 
-  double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+  for(int i = 0; i < SEARCH_NUM; i++) {
+    randN = rand() % NUM_ELEM + 1;
 
-  printf("Tempo da busca Linear: %f\n", time_spent);
+    clock_t begin = clock();
+      printLineLinear(f, randN);
+    clock_t end = clock();
+
+    time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+    mean += time_spent;
+  }
 
 
+
+  printf("Tempo total da busca Linear: %.4f\n", (double) mean);
+  printf("Tempo medio da busca Linear: %.4f\n", (double) mean / SEARCH_NUM);
+
+
+  fclose(f);
   return 0;
 }
 
