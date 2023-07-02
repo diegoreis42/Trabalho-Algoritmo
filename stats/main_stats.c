e180b33550845ee683681a38e4579cc85aa4223f
Diego <reisdiego144@gmail.com>
2023-07-02 20:02:06 -0300

    adicionando max e min na stats de busca linear

--- stats/main_stats.c
+++ stats/main_stats.c
@@ -17,11 +17,11 @@
 int main ()
 {
   srand(time(NULL));
-  double mean = 0.0, time_spent; 
+  double mean = 0.0, time_spent, max = 0, min = 0; 
   int randN;
   FILE *f = fopen(FILE_NAME, "r");
 
-
+// calcula a media, valor max e min para 30 buscas aleatorias
   for(int i = 0; i < SEARCH_NUM; i++) {
     randN = rand() % NUM_ELEM + 1;
 
@@ -31,9 +31,17 @@ int main ()
 
     time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
     mean += time_spent;
+
+    if (time_spent > max)
+      max = time_spent;
+
+    if (time_spent < min)
+      min = time_spent;
   }
 
-  printf("Tempo medio da busca Linear: %.4f\n", (double) mean / SEARCH_NUM);
+  printf("Tempo medio da busca Linear: %f\n", (double) mean / SEARCH_NUM);
+  printf("Menor tempo da busca Linear: %f\n", min);
+  printf("Maior tempo da busca Linear: %f\n", max);
 
 
   fclose(f);
